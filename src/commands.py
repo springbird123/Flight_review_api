420317757850c1508de6367ba514e1ef75115181
springbird123 <yubangkai1998@gmail.com>
2023-03-22 23:43:12 +1100

    add commands including seed the airport and user

--- src/commands.py
+++ src/commands.py
@@ -1,37 +1,55 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager
+from flask import Blueprint
+from main import db
+from datetime import date
+import csv
 
-db = SQLAlchemy()
-ma = Marshmallow()
-bcrypt = Bcrypt()
-jwt = JWTManager()
+from Models.user import User
+from Models.review import Review
+from Models.flight import Flight
+from Models.airport import Airport
 
 
-def create_app():
-    # Create Flask object
-    app = Flask(__name__)
+# Instantiate a blueprint for CLI database commands
+db_commands = Blueprint('db', __name__)
 
-    # Configure Flask
-    app.config.from_object("config.app_config")
+@db_commands.cli.command('create')
+def create_db():
+    db.create_all()
+    print("Tables created")
 
-    # Create SQLAlchemy, Marshmallow, JWT and Bcrypt objects
-    db.init_app(app)
-    ma.init_app(app)
-    bcrypt.init_app(app)
-    jwt.init_app(app)
+@db_commands.cli.command('drop')
+def drop_db():
+    db.drop_all()
+    print("Tables dropped")
 
-    # Import commands
-    from commands import db_commands
-    app.register_blueprint(db_commands)
+@db_commands.cli.command('seed')
 
-    # Import controllers
-    from controller import registerable_controllers
 
-    # Register blueprints
-    for controller in registerable_controllers:
-        app.register_blueprint(controller)
+def seed_db():
 
-    return app
+    # Open the airport CSV file
+    with open('./app/data/airport.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile)
+
+        # Iterate through each row in the CSV file
+        for row in reader:
+            # Create a new Airport object with the data from the row
+            airport = Airport(airport_id=row[0], airport_name=row[1], city=row[2], country=row[3], IATA=row[4], ICAO=row[5])
+
+            # Add the Airport object to the database session
+            db.session.add(airport)
+
+    # Commit the changes to the database
+    db.session.commit()
+
+    user1 = User(
+        username = 'qantas_lover',
+        email = "loveqantas@email.com",
+        password = bcrypt.generate_password_hash("password123").decode("utf-8"),
+    )
+    db.session.add(user1)
+
+
+
+
+    db.session.commit()
